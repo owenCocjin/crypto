fb0aa2e773ae685467a3b7b838191703abcbc231
Owen Cocjin <owen_jamescf@hotmail.com>
2020-01-14 23:25:11 -0500

    Added crypto_misc imports

--- CryptoPack/__init__.py
+++ CryptoPack/__init__.py
@@ -1,7 +1,8 @@
 ## Author:	Owen Cocjin
-## Version:	1.0
-## Date:	10/01/20
+## Version:	1.1
+## Date:	14/01/20
 ## Notes:
 
 from cryptoPack.crypto_classes import *
 from cryptoPack.crypto_funcs import *
+from cryptoPack.crypto_misc import *
