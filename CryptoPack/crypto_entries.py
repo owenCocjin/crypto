128d5371965f530b587c9f364c83527c529f3bf3
Owen Cocjin <owen_jamescf@hotmail.com>
2020-01-16 16:14:07 -0500

    Added generate flag

--- CryptoPack/crypto_entries.py
+++ CryptoPack/crypto_entries.py
@@ -4,11 +4,15 @@
 ## Notes:
 
 from progMenu import menu, MenuEntry
+from .crypto_misc import keyz26, patdown
 
 #-------------#
 #    FUNCS    #
 #-------------#
 def dFunc():
+	return True
+
+def gFunc():
 	return True
 
 def hFunc():
@@ -17,13 +21,14 @@ def hFunc():
 \033[33mArguments:\033[0m
 \t-d, --decrypt\tDecrypt instead of encrypt (default)
 \t-h, --help\tPrints this page
-\t-k, --key\tSets key based on mode. Takes from stdin if no arg
-\t-m, --mode\tSets encryption mode:
+\t-k, --key=<k>\tSets key based on mode. Takes from stdin if no arg
+\t-m, --mode=<m>\tSets encryption mode:
 \t\t\t\t- 0/shift: Shift Cipher
 \t\t\t\t- 1/affine: Affine Cipher
-\t\t\t\t- 2/vige: Vigenere Cipher
-\t\t\t\t- 3/trans: Transposition Cipher\n
-\t-w, --word\tSets the plaintext word. Takes from stdin if no arg
+\t\t\t\t- 2/vigenere: Vigenere Cipher
+\t\t\t\t- 3/transpo: Transposition Cipher
+\t\t\t\t- 4/rsa: RSA\n
+\t-w, --word=<w>\tSets the plaintext word. Takes from stdin if no arg
 ''')
 	exit(0)
 	return True
@@ -31,10 +36,13 @@ def hFunc():
 def kFunc(k=None):
 	'''Sets the key based on the mode'''
 	k=input("Key: ") if not k else k
-	mode=menu.sgetAssigned(['m', "mode"])
+	mode=menu.sgetAssigned(['m', "mode"]).lower()
 	#Try splitting into ints and stripping
 	try:
 		k=[int(i.strip()) for i in k.split(',')]
+	except ValueError:
+		#Turn into ints if string passed
+		k=keyz26(k)
 	except:
 		print("[|X: kFunc:KeyError]: Invalid key!")
 		exit()
@@ -42,20 +50,20 @@ def kFunc(k=None):
 	if mode in ['0', "shift"]:  #Single key
 		return k[0]
 
-	elif mode in ['1', "affine"]:  #Double key
+	elif mode in ['1', '2', "affine", "rsa"]:  #Double key
 		if len(k)<2:
 			print("[|X: kFunc:KeyError]: Invalid key (2 required)!")
 			exit()
 		else:
 			return k[:2]
 
-	elif mode in ['3', "transpo"]:  #Keys in a set (0..len(key))
+	elif mode in ['2', "transpo"]:  #Keys in a set (0..len(key))
 		for i in k:
 			if len(k)<i or i<0 or k.count(i)!=1:
 				print(f"[|X: kFunc:KeyError]: Invalid key!")
 				exit()
-			else:
-				return k
+		return k
+
 	else:
 		return k
 
@@ -63,12 +71,18 @@ def mFunc(m):
 	return m.lower()
 
 def wFunc(w=None):
-	return w if w else input("Word: ")
+	w=w if w else input("Word: ")
+	try:  #List of ints
+		return [int(i.strip()) for i in w.split(',')]
+	except Exception as e:  #String
+		print(e)
+		return patdown(w)
 
 #---------------#
 #    ENTRIES    #
 #---------------#
 dEntry=MenuEntry("decrypt", ['d', "decrypt"], dFunc, 0)
+gEntry=MenuEntry("generate", ['g', "generate"], gFunc, 0)
 hEntry=MenuEntry("help", ['h', "help"], hFunc, 0)
 kEntry=MenuEntry("key", ['k', "key"], kFunc, 3)
 mEntry=MenuEntry("mode", ['m', "mode"], mFunc, 1)
