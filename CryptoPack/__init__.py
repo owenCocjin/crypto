b9cfd0b5b0de61bd8e766549e9761a35685688eb
Owen Cocjin <owen_jamescf@hotmail.com>
2020-03-12 18:17:47 -0400

    Added crypto_errors

--- CryptoPack/__init__.py
+++ CryptoPack/__init__.py
@@ -1,7 +1,9 @@
 ## Author:	Owen Cocjin
-## Version:	1.1
-## Date:	14/01/20
+## Version:	1.2
+## Date:	17/02/20
 ## Notes:
+##	- Added crypto_errors
 
 from cryptoPack.crypto_classes import *
 from cryptoPack.crypto_funcs import *
+from cryptoPack.crypto_errors import *
